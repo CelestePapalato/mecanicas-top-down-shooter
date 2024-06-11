754a3fa67a93b65997c61759912890b86401a984
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2024-06-11 15:12:09 -0300

    Creada State Machine Enemy

---
+++ Assets/Script/State Machine/Machines/Enemy.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Enemy : StateMachine
+{
+    public static event Action<int> EnemyDead;
+
+    [SerializeField] int points;
+    public UnityAction<Enemy> OnDead;
+    Health healthComponent;
+    ItemSpawner itemSpawner;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        healthComponent = GetComponentInChildren<Health>();
+        if (healthComponent)
+        {
+            healthComponent.NoHealth += Dead;
+        }
+        itemSpawner = GetComponent<ItemSpawner>();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    private void Dead()
+    {
+        itemSpawner?.DropItem();
+        Destroy(gameObject);
+        EnemyDead?.Invoke(points);
+        //GameManager.instance.AddPoints(points);
+    }
+
+    private void OnDestroy()
+    {
+        OnDead?.Invoke(this);
+    }
+}
---
+++ Assets/Script/State Machine/StateMachine.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StateMachine : MonoBehaviour
+{
+    [SerializeField] protected Estado primerEstado;
+
+    protected Estado estadoActual;
+    protected Estado primerEstadoBuffer;
+    protected Estado ultimoEstado;
+
+    protected virtual void Awake()
+    {
+        if (!primerEstado)
+        {
+            primerEstado = GetComponent<Estado>();
+        }
+
+        if (primerEstado)
+        {
+            primerEstadoBuffer = primerEstado;
+            CambiarEstado(primerEstado);
+        }
+        else
+        {
+            Debug.LogWarning("El State Machine " + name + "no posee ni encuentra un Estado al que llamar");
+        }
+    }
+
+    protected virtual void Update()
+    {
+        if (estadoActual)
+        {
+            estadoActual.Actualizar();
+        }
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        if (estadoActual)
+        {
+            estadoActual.ActualizarFixed();
+        }
+    }
+
+    public virtual void CambiarEstado(Estado nuevoEstado)
+    {
+        estadoActual?.Salir();
+        estadoActual = (nuevoEstado) ? nuevoEstado : primerEstado;
+        estadoActual?.Entrar(this);
+    }
+
+    private void OnEnable()
+    {
+        if (estadoActual)
+        {
+            return;
+        }
+        primerEstado = primerEstadoBuffer;
+        CambiarEstado(ultimoEstado);
+    }
+
+    private void OnDisable()
+    {
+        ultimoEstado = estadoActual;
+        primerEstadoBuffer = primerEstado;
+        primerEstado = null;
+        estadoActual?.Salir();
+    }
+
+    private void OnDestroy()
+    {
+        primerEstado = null;
+        estadoActual?.Salir();
+    }
+}
+
+public abstract class Estado : MonoBehaviour
+{
+    protected StateMachine personaje;
+
+    public virtual void Entrar(StateMachine personajeActual)
+    {
+        personaje = personajeActual;
+    }
+    public virtual void Salir() { }
+    public virtual void Actualizar() { }
+    public virtual void ActualizarFixed() { }
+    public virtual void DañoRecibido() { }
+
+    private void OnDisable()
+    {
+        if (!personaje) { return; }
+        personaje.CambiarEstado(null);
+    }
+
+    private void OnDestroy()
+    {
+        if (!personaje) { return; }
+        personaje.CambiarEstado(null);
+    }
+}
